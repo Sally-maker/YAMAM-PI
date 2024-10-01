93e6e1b0bcb9e0c677a3218cb5aab936aac071aa
Richard Miranda São Bernardo <126610839+Rich-Miranda@users.noreply.github.com>
2024-10-01 13:33:02 -0300

    Add files via upload

--- src/pages/Home/script.js
+++ src/pages/Home/script.js
@@ -1,9 +1,27 @@
-function verifyPermissons(){
-    const role = localStorage.getItem("role")
-    const linkProducts = document.querySelector("#link_products")
-    console.log(role)
-    if(role =='STOCKIST'){
-        linkProducts.style.display = 'none'
-      
-    }   
-}
+let navbar = document.querySelector(".fa-user");
+document.querySelector("#user-btn").onclick = () => {
+  navbar.classList.toggle("active");
+  seachForm.classList.remove("active");
+  cartItem.classList.remove("active");
+};
+
+let seachForm = document.querySelector(".search-form");
+document.querySelector("#search-btn").onclick = () => {
+  seachForm.classList.toggle("active");
+  navbar.classList.remove("active");
+  cartItem.classList.remove("active");
+};
+
+let cartItem = document.querySelector(".cart-items-container");
+document.querySelector("#cart-btn").onclick = () => {
+  cartItem.classList.toggle("active");
+  navbar.classList.remove("active");
+  seachForm.classList.remove("active");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+  seachForm.classList.remove("active");
+  cartItem.classList.remove("active");
+};
+
